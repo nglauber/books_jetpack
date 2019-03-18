9a156c3a52c67b12c17ead829c7c8cf81d18f907
Amadeu Cavalcante <amadeu01@gmail.com>
2019-03-18 18:48:41 -0300

    Refactor
    Replace IllegalArgumentException by kotlin require function
    https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/require.html

--- domain/src/main/java/dominando/android/domain/interactor/ViewBookDetailsUseCase.kt
+++ domain/src/main/java/dominando/android/domain/interactor/ViewBookDetailsUseCase.kt
@@ -5,16 +5,12 @@ import dominando.android.data.model.Book
 import dominando.android.domain.FlowableUseCase
 import dominando.android.domain.executor.PostExecutionThread
 import io.reactivex.Flowable
-import java.lang.IllegalArgumentException
 
 open class ViewBookDetailsUseCase(private val repository: BooksRepository,
                        postExecutionThread: PostExecutionThread
 ) : FlowableUseCase<Book, String>(postExecutionThread) {
     override fun buildUseCaseFlowable(params: String?): Flowable<Book> {
-        if (params != null) {
-            return repository.loadBook(params)
-        } else {
-            throw IllegalArgumentException("You must pass a book id")
-        }
+        require(params != null) { "You must pass a book id" }
+        return repository.loadBook(params)
     }
 }
